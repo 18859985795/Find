4e260d528f0bdf4bf824a8b6f57aa17e02ff5cf1
nulinulinuli <1034678158@qq.com>
2022-07-05 10:25:27 +0800

    地图模块和上拉框模块布局

---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="view">
+		<map class="map"></map>
+		<upFrame></upFrame>
+	</view>
+		
+</template>
+
+<script>
+	import upFrame from "@/components/upFrame/upFrame.vue"
+	export default {
+		name: 'index',
+		data() {
+			return {
+				
+			}
+		},
+		components:{upFrame},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	page {
+		height: 100%;
+	}
+	.view{
+		height: 100%;
+		width: 100%;
+	}
+	.map{
+		height: 100%;
+		width: 100%;
+		background-color: #f7f7f7;
+	}
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="">
+		嘿嘿
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "my",
+		data(){
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
